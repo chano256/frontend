4a77bd837b960a386b1013a0aa33991b477d3cda
Chano <allanochan8@gmail.com>
2025-02-09 13:28:35 +0300

    switches to using vite

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,9 +2,9 @@ import api from "./axios";
 
 export const login = async (credentials) => {
   return await api.post("/auth/login", {
-    grant_type: process.env.VUE_APP_API_PASSPORT_GRANT_TYPE,
-    client_id: process.env.VUE_APP_API_PASSPORT_CLIENT_ID, // Store in .env
-    client_secret: process.env.VUE_APP_API_PASSPORT_CLIENT_SECRET, // Store in .env
+    grant_type: import.meta.env.VITE_API_PASSPORT_GRANT_TYPE,
+    client_id: import.meta.env.VITE_API_PASSPORT_CLIENT_ID, // Store in .env
+    client_secret: import.meta.env.VITE_API_PASSPORT_CLIENT_SECRET, // Store in .env
     username: credentials.username,
     password: credentials.password,
     scope: "*",
